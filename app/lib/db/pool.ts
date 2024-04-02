2607d4c1b7ee5030dd98f95f614a627fda1253b7
KhaledElmorsy <khaled.mg.a@gmail.com>
2024-04-03 00:31:28 +0200

    feat: Add postgres url env variable for vercel

--- app/lib/db/pool.ts
+++ app/lib/db/pool.ts
@@ -1,11 +1,14 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT as string, 10),
-});
+const pool =
+  process.env.NODE_ENV === 'production'
+    ? new Pool({ connectionString: process.env.POSTGRES_URL })
+    : new Pool({
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        port: parseInt(process.env.PGPORT as string, 10),
+      });
 
 export default pool;
